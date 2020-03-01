c0032146e2a02fdf0d7b21a8a18168c7c1ce8b2d
ozada <oskar.huseynzada@wirecard.com>
2020-03-01 14:17:44 +0100

    Test inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 x=${INPUT_PARAM_X}
 y=${INPUT_PARAM_Y}
 z=$((x + y))
-echo "Hello $1"
+echo "Hello ${INPUT_GREET_PERSON_NAME}"
 echo "X: ${x} / Y: ${y}"
 time=$(date)
 echo ::set-output name=time::"${time}"
