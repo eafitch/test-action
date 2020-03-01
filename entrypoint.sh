b2d3787a758e5a2cbfda44eb344f7b5598abaa94
ozada <oskar.huseynzada@wirecard.com>
2020-03-01 13:46:26 +0100

    Test inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
-# shellcheck disable=SC2068
-echo echo $@;
-echo "Hello $1"
+echo "Hello ${INPUT_WHO-TO-GREET}"
+echo "X: ${INPUT_PARAM-X} / Y: ${INPUT_PARAM-Y}"
 time=$(date)
 echo ::set-output name=time::"${time}"
