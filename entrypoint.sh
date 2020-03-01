4ab96d54c8a854b3cf12a933e22c71b3c0bb9830
ozada <oskar.huseynzada@wirecard.com>
2020-03-01 13:50:06 +0100

    Test inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
 
 env
-echo "Hello ${INPUT_WHO-TO-GREET}"
-echo "X: ${INPUT_PARAM-X} / Y: ${INPUT_PARAM-Y}"
+echo $INPUT_PARAM-X
+echo $INPUT_PARAM-Yx
+echo "Hello " + $INPUT_WHO-TO-GREET
+echo "X: $INPUT_PARAM-X / Y: ${INPUT_PARAM-Y}"
 time=$(date)
 echo ::set-output name=time::"${time}"
